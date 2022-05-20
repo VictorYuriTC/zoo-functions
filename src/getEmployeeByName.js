d9f2438a113d24dbb62af923d864ea4fff0dc48e
Victor Yuri <victoryuritc@yahoo.com>
2022-05-20 20:01:22 -0300

    Início de tentativa de resolução do exercício getEmployeeByName.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const allFirstNames = employees.map((employee) => employee.firstName);
+  console.log(allFirstNames);
+  const allLastNames = employees.map((employee) => employee.lastName);
+  console.log(allLastNames);
+  const allNames = [...allFirstNames, ...allLastNames];
+  console.log(allNames);
+  return allNames.map((name) => name === employeeName);
 }
+
+console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
