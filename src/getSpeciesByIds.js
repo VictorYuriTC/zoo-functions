69d1c6bb3fc229ab9740ee29eb5bbc6934bddd9e
Victor Yuri <victoryuritc@yahoo.com>
2022-05-20 19:08:29 -0300

    Resolução de 2 dos 3 quesitos do desafio 1. Filter para encontrar o id e utilização de index no ids.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-
+  if (ids.length <= 1) {
+    return data.species.filter((specie) => specie.id === ids[0]);
+  }
 }
 
+console.log(getSpeciesByIds('78460a91-f4da-4dea-a469-86fd2b8ccc84'));
 module.exports = getSpeciesByIds;
-
-const numbers = [1, 2, 3, 4, 5];
-
-const evenNumbers = (arrayWithNumbers) => arrayWithNumbers.filter((number) => number % 2 === 0);
-
-console.log(evenNumbers(numbers));
