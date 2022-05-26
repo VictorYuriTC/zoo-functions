ac540631fc1a3a772b5deb20c7b4172d9d2355dd
Victor Yuri <victoryuritc@yahoo.com>
2022-05-26 03:49:24 -0300

    Resolução do getSpeciesById com utilização do includes posteriormente ao parâmetro.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,15 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
+  console.log(...ids);
   if (ids.length <= 1) {
     return data.species.filter((specie) => specie.id === ids[0]);
   }
   if (ids.length > 1) {
-    return data.species.filter((specie) => specie.id === ids[0]);
+    return data.species.filter((specie) => ids.includes(specie.id));
   }
 }
 
 console.log(getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274',
-  '0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
