aeecc1bfd59d33c0b4f2098cb518d72feaa4ce1c
Victor Yuri <victoryuritc@yahoo.com>
2022-05-26 02:23:49 -0300

    100% de cobertura de testes para o handlerElephants.

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,4 +10,16 @@ describe('Testes da função HandlerElephants', () => {
   it('returns the correct elephants age average when called', () => {
     expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
+  it('returns the correct popularity when called', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('returns the correct message when the parameter is not a string', () => {
+    expect(handlerElephants(true)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('returns undefined when the parameter is undefined', () => {
+    expect(handlerElephants().toBe(undefined));
+  });
+  it('returns the correct location when called', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
 });
