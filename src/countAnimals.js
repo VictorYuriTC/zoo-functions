a10f898d831b0f351356c48dc4ec834fe63df8e2
Victor Yuri <victoryuritc@yahoo.com>
2022-05-27 17:29:30 -0300

    Ajuste do linter.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,11 +1,7 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  if (animal === undefined) {
 
-  }
 }
-
-console.log(countAnimals());
 
 module.exports = countAnimals;
