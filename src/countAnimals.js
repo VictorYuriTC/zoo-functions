4f3bccf884de844423bd79a6b6f1b646e9f444f8
Victor Yuri <victoryuritc@yahoo.com>
2022-05-31 00:33:12 -0300

    Resolução do desafio countAnimals com utilização de reduce para sintetizar as informações em um só accumulator. Utilização de spread operar dentro da arrow function. Utilização de colchetes para utilização o specie.name como index. Utilização de operadores lógicos para analisar o objeto passado como argumento.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-
+  if (animal === undefined) {
+    return data.species.reduce((accumulator, specie) => (
+      { ...accumulator, [specie.name]: specie.residents.length }
+    ), '');
+  }
+  if (animal.specie !== undefined && animal.sex !== undefined) {
+    return data.species.find((specie) =>
+      specie.name === animal.specie).residents.filter((resident) =>
+      resident.sex === animal.sex).length;
+  }
+  if (animal !== undefined) {
+    return data.species.find((specie) => specie.name === animal.specie).residents.length;
+  }
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
