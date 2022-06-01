b0a779cd96fe62ac9baae866efdcb055be3a4e23
Victor Yuri <victoryuritc@yahoo.com>
2022-06-01 18:47:49 -0300

    Finalmente a resolução do desafio getEmployeesCoverage. Criação da função allEmployeesInfos para recuperar as informações de todos os employees e posterior utilização de map onde cada employee era argumento das funções que buscavam species e location. Utilização de if para criar a condição onde o argumento da função getEmployeesCoverage era undefined e, então, retorno da função allEmployeesInfos.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,6 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeResponsabilities(employeeNameOrId) {
+  if (employeeNameOrId === undefined) {
+    return data.employees.map((employee) => employee.responsibleFor);
+  }
   const selectedEmployee = data.employees.find((employee) =>
     employee.firstName === employeeNameOrId.name
     || employee.lastName === employeeNameOrId.name || employee.id === employeeNameOrId.id);
@@ -25,10 +28,24 @@ function getResponsabilitiesSpecies(employeeNameOrId) {
   return responsabilitiesSpecies;
 }
 
+function allEmployeesInfos() {
+  const allEmployeesIds = data.employees.map((employee) => (
+    { id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: getResponsabilitiesSpecies(employee),
+      locations: getResponsabilitiesLocations(employee),
+    }
+  ));
+  return allEmployeesIds;
+}
+
 /* Source: https://stackoverflow.com/questions/455338/how-do-i-check-if-an-object-has-a-key-in-javascript */
 
 function getEmployeesCoverage(employeeNameOrId) {
   const employeeObject = employeeNameOrId;
+  if (employeeObject === undefined) {
+    return allEmployeesInfos();
+  }
   const species = getResponsabilitiesSpecies(employeeObject);
   const locations = getResponsabilitiesLocations(employeeObject);
   const selectedEmployee = data.employees.find((employee) =>
@@ -41,6 +58,6 @@ function getEmployeesCoverage(employeeNameOrId) {
 
 const randomObject = { name: 'Nelson' };
 
-console.log(getEmployeesCoverage(randomObject));
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
